99ac2947199e752a486f29a24f490c164d1d383a
rzamolo- <rzamolo-@student.42madrid.com>
2025-05-23 15:33:55 +0200

    starting to create builtin functions

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 11:44:00 by rzamolo-          #+#    #+#             */
-/*   Updated: 2025/05/23 12:52:51 by rzamolo-         ###   ########.fr       */
+/*   Updated: 2025/05/23 14:37:34 by rzamolo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,38 @@
 #include <signal.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/wait.h>
 
-#define PROMPT "$>"
+#define PROMPT "$> "
+
+typedef int (*builtin_fn)(char **args);
+
+typedef struct s_builtin
+{
+	const char	*cmd; // cmd, e.g. "cd"
+	builtin_fn	function; // pointer to command function
+}	t_builtin;
+
+typedef struct s_redir
+{
+	int		type; // IN, OUT, APPEND
+	char	*file; // file 
+}	t_redir;
+
+typedef struct s_cmd
+{
+	char			**args; // string vector [command, arg1, ..., NULL]
+	t_redir			*in; // input list redirection
+	t_redir			*out; // output list redirection
+	struct	s_cmd	*next; // pipeline
+}	t_cmd;
+
+int	builtin_cd(char **args);
+int	builtin_echo(char **args);
+int	builtin_pwd(char **args);
+int	builtin_export(char **args);
+int	builtin_unset(char **args);
+int	builtin_env(char **args);
+int	builtin_exit(char **args);
 
 #endif

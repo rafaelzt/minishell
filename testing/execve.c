2463cd37aa9e7713475e681eff222f409532c85d
rzamolo- <rzamolo-@student.42madrid.com>
2025-06-02 12:30:08 +0200

    not clear how to list environment, trying to use (env)

--- testing/execve.c
+++ testing/execve.c
@@ -6,25 +6,30 @@ int main(void)
 {
 	int		i = 0;
 	char	*lst[] = {"user1", "Hello", "World", NULL};
-	char	*lst2[] = {"env", "Hello", "World", NULL};
-	char	*envp[] = {"USER=someuser", "PATH=/custom/path", "DEBUG=1", NULL};
+	char	*lst2[] = {"env", "ls", "/home/rzamolo-/Documents/minishell/testing", NULL};
+	char	*envp[] = {"USER=someuser", "PATH=/usr/bin/", "DEBUG=1", NULL};
 	pid_t	pid;
 
+	printf("Main program started\n");
 	pid = fork();
-	printf("Main program started [%d]\n", pid);
 	if (pid == 0)
 	{
 		if (execve("/bin/echo", lst, envp) == -1) // argv passed to /bin/echo, but some programs, like `echo` ignore the index = 0;
-			perror("Could not execve");
-		execve("/bin/echo", &lst[0], envp); // this execution is ignored
+			perror("Could not execve [echo #1]");
 	}
 	wait(NULL);
 	pid = fork();
 	if (pid == 0)
-	{
-		if (execve("/usr/bin/env", lst, envp) == -1)
-			perror("Could not execve");
-	}
+		if (execve("/bin/echo", &lst[0], envp) == -1)
+				perror("Could not execve [echo #2]");
+// If the execve() work properly, the process is changed to /bin/echo and my main process doesn't exist anymore.
+// to workaround that we use forks
 	wait(NULL);
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execve("/usr/bin/env", lst2, envp) == -1)
+			perror("Could not execve [env]");
+	}
 	return (0);
 }
